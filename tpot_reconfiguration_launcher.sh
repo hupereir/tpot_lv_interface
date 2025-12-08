86688be8d42894c23b473b050027f85d58dbb3f6
mchiu-bnl <chiu@bnl.gov>
2025-12-08 13:30:51 -0500

    improved error message

--- tpot_reconfiguration_launcher.sh
+++ tpot_reconfiguration_launcher.sh
@@ -3,7 +3,7 @@ echo "Running TPOT Reconfiguration Wizard on remote host ebdc39"
 ssh -t phnxrc@ebdc39.sphenix.bnl.gov  "bash -l /home/phnxrc/operations/TPOT/tpot_lv_interface/tpot_reconfiguration_wizard.sh"  | tee -a /home/phnxrc/operations/TPOT/logs/tpot_remote_launch_$(date +%Y%m%d_%H%M%S).log
 ssh_exit=${PIPESTATUS[0]}
 if [[ $ssh_exit -ne 0 ]]; then
-  echo "Could not connect to ebdc39 or remote operation failed. Call expert."
+  echo "Reconfiguration failed. Call expert."
   exit 1
 fi
 echo "Remote reconfiguration complete. Connection closed."
